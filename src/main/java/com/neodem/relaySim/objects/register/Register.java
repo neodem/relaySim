6814c39fdd9d191d430e3852b01d1c2a6e9a70ae
Vincent Fumo <neodem@gmail.com>
2016-03-27 09:36:01 -0400

    * adds register code

--- src/main/java/com/neodem/relaySim/objects/register/Register.java
+++ src/main/java/com/neodem/relaySim/objects/register/Register.java
@@ -1,5 +1,6 @@
 package com.neodem.relaySim.objects.register;
 
+import com.neodem.relaySim.objects.BitField;
 import com.neodem.relaySim.objects.Component;
 import com.neodem.relaySim.objects.Connector;
 
@@ -8,13 +9,23 @@ import com.neodem.relaySim.objects.Connector;
  * Created on : 3/25/16
  */
 public class Register implements Component {
-    private Connector value;
+    private BitField input;
+    private BitField register;
 
-    public Connector getValue() {
-        return value;
+    public Register(int size) {
+        input = new BitField(size);
+        register = new BitField(size);
     }
 
-    public void setValue(Connector in) {
-        this.value = in;
+    public BitField get() {
+        return register;
+    }
+
+    public void set(BitField value) {
+        this.input = value;
+    }
+
+    public void store() {
+        this.register = new BitField(this.input);
     }
 }
