1ff5e8b736d42a07fcb00e28b9b63549e64d14e4
Vincent Fumo <neodem@gmail.com>
2016-04-03 21:43:51 -0400

    * stuff

---
+++ src/mainPjava/com/neodem/relaySim/objects/bus/BusFactory.java
@@ -0,0 +1,22 @@
+package com.neodem.relaySim.objects.bus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Vincent Fumo (neodem@gmail.com)
+ * Created on 3/27/16
+ */
+public class BusFactory {
+    private Map<String, Bus> busses = new HashMap<>();
+
+    public Bus getBus(String id, int size) {
+        Bus bus = busses.get(id);
+        if(bus == null) {
+            bus = new Bus(size);
+            busses.put(id, bus);
+        }
+
+        return bus;
+    }
+}
---
+++ src/mainPjava/com/neodem/relaySim/objects/register/Register.java
@@ -0,0 +1,29 @@
+package com.neodem.relaySim.objects.register;
+
+import com.neodem.relaySim.objects.BitField;
+
+/**
+ * Created by : Vincent Fumo (neodem@gmail.com)
+ * Created on : 3/25/16
+ */
+public class Register {
+    private BitField input;
+    private BitField register;
+
+    public Register(int size) {
+        input = new BitField(size);
+        register = new BitField(size);
+    }
+
+    public BitField get() {
+        return register;
+    }
+
+    public void set(BitField value) {
+        this.input = value;
+    }
+
+    public void store() {
+        this.register = new BitField(this.input);
+    }
+}
